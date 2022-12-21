85f294ded42df4cb9db9523714902978837d7ef4
jjeansp9 <jjeansp9@gmail.com>
2022-12-21 14:31:23 +0900

    commit

---
+++ Ex05Layout/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {View, Text, TextInput, Image, Button, Alert} from 'react-native'
+
+export default class Main extends Component{
+    render(){
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
